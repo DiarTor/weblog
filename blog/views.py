19bd0e1dd000b25ecb6d54de1a6abdbf8046a292
debland <hosseinnysyan1385@gmail.com>
2023-04-06 21:27:42 +0330

    add font and fix bugs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home (request):
-    return render(request, 'blog/home.html')
+def home(request):
+    return render(request, "blog/home.html")
