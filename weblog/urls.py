0e0eff39a012a5602427fe4e2b5037a33a00c9ec
debland <hosseinnysyan1385@gmail.com>
2023-04-06 22:01:42 +0330

    fix url bugs and add some include

--- weblog/urls.py
+++ weblog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from blog import views
+from django.urls import path , include
+from blog import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="homepage")
+    path('', include(urls)),
+
+
 ]
