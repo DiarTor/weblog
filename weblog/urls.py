00c27ebadac0834f68780928e57528502558048e
DiarTor <mhmmd2020.15@gmail.com>
2023-04-08 00:53:48 +0430

    Fixed URL Bug

--- weblog/urls.py
+++ weblog/urls.py
@@ -19,7 +19,6 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('account/' , include('users.urls')),
 
 
 ]
