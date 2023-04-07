a3cfb7222ef61ec081a2d5a6e325f2d6fb76a410
DiarTor <mhmmd2020.15@gmail.com>
2023-04-07 21:46:16 +0430

    added users app instead of auth1

--- auth1/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import login, authenticate
-
-# Create your views here.
-def signup(request):
-    return render(request, "auth1/signup.html" ,)
--- weblog/settings.py
+++ weblog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'auth1',
+    'users',
 ]
 
 MIDDLEWARE = [
