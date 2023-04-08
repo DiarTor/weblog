eb98b08bc144bff8285ddde1f7fca25075e53415
DiarTor <mhmmd2020.15@gmail.com>
2023-04-08 21:57:06 +0430

    Added SignUp Feature

--- users/views.py
+++ users/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from users.forms import RegisterForm
 
 # Create your views here.
-def signup(request):
+def sign_up(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home-page')
     else:
         form = RegisterForm()
     return render(request, 'registration/sign_up.html', {'form': form})
-def login(request):
+def log_in(request):
     return render(request, 'users/templates/registration/login.html')
