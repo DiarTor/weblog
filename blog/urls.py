10c81df7a9ccd32e3f387c3ddf4903d6c7a298f9
DiarTor <mhmmd2020.15@gmail.com>
2023-04-08 23:30:40 +0430

    Added Create Post Feat

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from blog import views
 urlpatterns = [
-    path('', views.home, name="home-page")
-
+    path('', views.home, name="home-page"),
+    path('create-post/', views.create_post, name="create_post")
 ]
