9bc53ed52c4d99c97ce7f9a640b9cd85d505d998
debland <hosseinnysyan1385@gmail.com>
2023-04-08 22:42:49 +0330

    add box of posts

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .forms import PostForm
 # Create your views here.
 # @login_required(login_url="/account/login/")
 def home(request):
     return render(request, "blog/home.html")
-@login_required(login_url="/account/login/")
-def create_post(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect("home-page")
-    else:
-        form = PostForm()
-    return render(request, 'blog/create_post.html', {'form':form})
+
+
--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,4 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
+
