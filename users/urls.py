6001e2a6a90d4b8c99252cd1a51a94f3a3e090d4
DiarTor <mhmmd2020.15@gmail.com>
2023-04-08 20:36:26 +0430

    Added SignUp View

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from users import views
-urlpatters = [
+urlpatterns = [
     path('signup/', views.signup, name="signup")
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def signup(request):
+    return HttpResponse("<p> Hi </p>")
