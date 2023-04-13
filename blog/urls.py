d1b99b46a80956d035b927231f7366ca2f813bdd
DiarTor <mhmmd2020.15@gmail.com>
2023-04-13 21:45:16 +0430

    Created comments.html,button,view and url

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name="home-page"),
     path('create-post/', views.create_post, name="create_post"),
-    path('like/', views.like_post, name="like-post")
+    path('like/', views.like_post, name="like-post"),
+    path('<int:post_id>/comments/', views.comments, name='comments')
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post, Like
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -16,6 +17,19 @@ def home(request):
             post.delete()
     return render(request, "blog/home.html", {'posts': posts})
 
+
+@login_required(login_url="/account/login/")
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("home-page")
+    else:
+        form = PostForm()
+    return render(request, 'blog/create_post.html', {'form': form})
 
 def like_post(request):
     user = request.user
@@ -35,16 +49,6 @@ def like_post(request):
     like.save()
     return redirect("home-page")
 
-
 @login_required(login_url="/account/login/")
-def create_post(request):
-    if request.method == "POST":
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect("home-page")
-    else:
-        form = PostForm()
-    return render(request, 'blog/create_post.html', {'form': form})
+def comments(request, post_id):
+    return render(request, 'blog/comments.html')
