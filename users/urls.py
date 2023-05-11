4bb6552623d9ef4a47698de1eb9c9d79686982f4
Mostafa Mosavi <mosydev2016@gmail.com>
2023-05-11 19:58:22 +0430

    Classified users/views.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from users import views
+
+from . import views
+from .views import UserProfile, EditProfile, SignUp
+
 urlpatterns = [
-    path('profile/', views.profile, name="profile"),
-    path('sign-up/', views.sign_up, name='sign_up'),
-    path('edit-profile', views.edit_profile, name='edit_profile'),
-    # path('change-username/', views.change_username, name='change_username'),
-    path('change-password/', views.CustomPasswordChangeView.as_view(template_name='registration/password-change.html'), name='change_password')
+    path('profile/', UserProfile.as_view(), name="profile"),
+    path('sign-up/', SignUp.as_view(), name='sign_up'),
+    path('edit-profile/', EditProfile.as_view(), name='edit_profile'),
+    path('change-password/', views.CustomPasswordChangeView.as_view(template_name='registration/password-change.html'),
+         name='change_password')
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,13 @@
-from django.core.exceptions import ValidationError
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordChangeView
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from users.forms import RegisterForm, EditProfileForm
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import check_password
-from django.contrib import messages
-from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from django.views import View
+
+from users.forms import RegisterForm, EditProfileForm
 
 
 # Create your views here.
@@ -16,79 +16,69 @@ class CustomPasswordChangeView(PasswordChangeView):
     success_url = reverse_lazy('home-page')
 
 
-def profile(request):
-    return render(request, 'users/profile.html')
+class UserProfile(View):
+    def get(self, request):
+        return render(request, "users/profile.html")
 
-def edit_profile(request):
-    if request.method == "POST":
-        form = EditProfileForm(request.POST)
-        if form.is_valid():
-            # Change Username
-            if not request.POST['username'] == '':
-                try:
-                    user_username = request.user
-                    user_username.username = request.POST['username']
-                    user_username.save()
-                    return redirect('edit_profile')
-                except IntegrityError:
-                    messages.error(request, 'Username Is Invalid Please Try Another One.')
-                    form = EditProfileForm()
-            # Change First Name
-            if not request.POST['fname'] == '':
-                user_fname = User.objects.get(first_name=request.user.first_name)
-                user_fname.first_name = request.POST['fname']
-                user_fname.save()
-                return redirect('edit_profile')
-            # Change Last Name
-            if not request.POST['lname'] == '':
-                user_lname = User.objects.get(last_name=request.user.last_name)
-                user_lname.last_name = request.POST['lname']
-                user_lname.save()
-                return redirect('edit_profile')
-            # Change Email
-            if not request.POST['email'] == '':
-                user_email = User.objects.get(email=request.user.email)
-                user_email.email = request.POST['email']
-                user_email.save()
-                return redirect('edit_profile')
-    else:
+
+class EditProfile(View):
+    def get(self, request):
         form = EditProfileForm()
-    return render(request, 'users/edit_profile.html', {'form': form})
+        return render(request, 'users/edit_profile.html', {'form': form})
+
+    def post(self, request):
+        if request.method == "POST":
+            form = EditProfileForm(request.POST)
+            if form.is_valid():
+                # Change Username
+                if not request.POST['username'] == '':
+                    try:
+                        user_username = request.user
+                        user_username.username = request.POST['username']
+                        user_username.save()
+                        return redirect('edit_profile')
+                    except IntegrityError:
+                        messages.error(request, 'Username Is Invalid Please Try Another One.')
+                        form = EditProfileForm()
+                # Change First Name
+                if not request.POST['fname'] == '':
+                    user_fname = User.objects.get(first_name=request.user.first_name)
+                    user_fname.first_name = request.POST['fname']
+                    user_fname.save()
+                    return redirect('edit_profile')
+                # Change Last Name
+                if not request.POST['lname'] == '':
+                    user_lname = User.objects.get(last_name=request.user.last_name)
+                    user_lname.last_name = request.POST['lname']
+                    user_lname.save()
+                    return redirect('edit_profile')
+                # Change Email
+                if not request.POST['email'] == '':
+                    user_email = User.objects.get(email=request.user.email)
+                    user_email.email = request.POST['email']
+                    user_email.save()
+                    return redirect('edit_profile')
+        else:
+            form = EditProfileForm()
+        return render(request, 'users/edit_profile.html', {'form': form})
 
 
-
-def sign_up(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home-page')
-    else:
+class SignUp(View):
+    def get(self, request):
         form = RegisterForm()
-    return render(request, 'registration/sign_up.html', {'form': form})
+        return render(request, 'registration/sign_up.html', {'form': form})
+
+    def post(self, request):
+        if request.method == "POST":
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                return redirect('home-page')
+        else:
+            form = RegisterForm()
+        return render(request, 'registration/sign_up.html', {'form': form})
 
 
 def log_in(request):
     return render(request, 'users/templates/registration/login.html')
-
-
-# def change_username(request):
-#     if request.method == "POST":
-#         form = ChangeUsernameForm(request.POST)
-#         if form.is_valid():
-#             # if ChangeUsernameForm.check_exist:
-#             #     messages.error(request, 'Username Is Invalid Please Try Another One.')
-#             #     form = ChangeUsernameForm()
-#             # else:
-#             try:
-#                 user = request.user
-#                 user.username = request.POST['new_username']
-#                 user.save()
-#                 return redirect('home-page')
-#             except IntegrityError:
-#                 messages.error(request, 'Username Is Invalid Please Try Another One.')
-#                 form = ChangeUsernameForm()
-#     else:
-#         form = ChangeUsernameForm()
-#     return render(request, 'users/change_username.html', {'form': form})
