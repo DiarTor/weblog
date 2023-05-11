ca2a4cf5d4d7b0565b68daefbd7b2e2ffef341be
Mostafa Mosavi <mosydev2016@gmail.com>
2023-05-11 20:08:01 +0430

    Fixed some syntax warnings(fname,lname,etc)

--- users/forms.py
+++ users/forms.py
@@ -15,15 +15,10 @@ class RegisterForm(UserCreationForm):
 
 class EditProfileForm(forms.Form):
     username_help_text = "Enter Your New Username"
-    fname_help_text = "Enter Your New First Name"
-    lname_help_text = "Enter Your New Last Name"
+    first_name_help_text = "Enter Your New First Name"
+    last_name_help_text = "Enter Your New Last Name"
     email_help_text = "Enter Your New Email"
     username = forms.CharField(max_length=50, help_text=username_help_text, required=False, label="Username :")
-    fname = forms.CharField(max_length=50, help_text=fname_help_text, required=False, label="First Name :")
-    lname = forms.CharField(max_length=50, help_text=lname_help_text, required=False, label="Last Name :")
+    first_name = forms.CharField(max_length=50, help_text=first_name_help_text, required=False, label="First Name :")
+    last_name = forms.CharField(max_length=50, help_text=last_name_help_text, required=False, label="Last Name :")
     email = forms.EmailField(max_length=50, help_text=email_help_text, required=False, label="Email :")
-
-    @property
-    def check_exist(self):
-        if User.objects.filter(username=self.new_username).exists():
-            return True
